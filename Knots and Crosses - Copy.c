732612a53e4935d7f498a901e0f2b6566ad16a5d
U9601 <ctjones1965@gmail.com>
2019-03-12 01:31:07 +0000

    removed a delete node method

--- Knots and Crosses - Copy.c
+++ Knots and Crosses - Copy.c
@@ -42,53 +42,6 @@ struct Node *addBegin(struct Node *last, int data)
   return last;
 }
 
-void deleteNode(struct Node *head, int key)
-{
-    if (head == NULL)
-        return;
-
-    struct Node *curr = head, *prev;
-    while (curr->data != key)
-    {
-        if (curr->next == head)
-        {
-            printf("\nGiven node is not found"
-                   " in the list!!!");
-            break;
-        }
-        prev = curr;
-        curr = curr -> next;
-    }
-
-    if (curr->next == head)
-    {
-        head = NULL;
-        free(curr);
-        return;
-    }
-
-    if (curr == head)
-    {
-        prev = head;
-        while (prev -> next != head)
-            prev = prev -> next;
-        head = curr->next;
-        prev->next = head;
-        free(curr);
-    }
-
-    else if (curr -> next == head)
-    {
-        prev->next = head;
-        free(curr);
-    }
-    else
-    {
-        prev->next = curr->next;
-        free(curr);
-    }
-}
-
 void reverse(struct Node* last)
 {
     if (last == NULL)
